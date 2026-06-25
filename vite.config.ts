e8b7fd142ea1d1203c1098d7586a85c13bc088b4
Vladyslav Bondarenko <vlad@medentee.com>
2026-06-25 02:53:14 +0200

    chore: set production base path

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base:
+    process.env.NODE_ENV === "production" ? "/panini_wc_2026_checklist/" : "/",
 });
